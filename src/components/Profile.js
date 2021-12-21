d93d173d404b84a664119837629e723ac4814646
Julian Swerdlin <julianswer36@gmail.com>
2021-12-21 20:28:47 +0100

    uploading files

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -9,8 +9,8 @@ const Profile = () => {
         <>
             {
                 selectedUser ? (
-                    <div>
-                        <img src={selectedUser.avatar} />
+                    <div className="card card-body text-center">
+                        <img src={selectedUser.avatar} className="card-img-top rounded-circle m-auto img-fluid" style={{width: 180}} />
                         <h1>{`${selectedUser.first_name} ${selectedUser.last_name}`}</h1>
                         <h3>{`${selectedUser.email}`}</h3>
                     </div>
