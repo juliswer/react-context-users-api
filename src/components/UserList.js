8a136ca08429ad653777b2d3099bd2f3cc0cabd3
Julian Swerdlin <julianswer36@gmail.com>
2021-12-21 20:21:12 +0100

    uploading files

--- src/components/UserList.js
+++ src/components/UserList.js
@@ -1,25 +1,30 @@
-import React, {useEffect, useContext} from 'react'
-import UserContext from '../context/User/UserContext';
+import React, { useEffect, useContext } from "react";
+import UserContext from "../context/User/UserContext";
 
 const UserList = () => {
+  const { users, getUsers } = useContext(UserContext);
 
-    const {users, getUsers} = useContext(UserContext)
+  useEffect(() => {
+    getUsers();
+  }, []);
 
-    useEffect(() => {
-        getUsers()
-    }, [])
+  return (
+    <div>
+      <div className="list-group h-100">
+        {users.map((user) => (
+          <a
+            className="list-group-item list-group-item-action d-flex flex-row justify-content-start"
+            onClick={() => console.log(user.id)}
+            href="#!"
+            key={user.id}
+          >
+              <img src={user.avatar} className="img-fluid mr-4 rounded-circle" width="70" />
+              <p>{`${user.first_name} ${user.last_name}`}</p>
+          </a>
+        ))}
+      </div>
+    </div>
+  );
+};
 
-    return (
-        <div>
-            <div className="list-group h-100">
-                {
-                    users.map(user => (
-                        <a key={user.id}>{`${user.first_name} ${user.last_name}`}</a>
-                    ))
-                }
-            </div>
-        </div>
-    )
-}
-
-export default UserList
+export default UserList;
